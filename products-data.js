f47cc03476063142dcccb7bd2c3588f9125d548c
vdk29 <Gagarin.vdk@yandex.ru>
2026-08-23 15:47:42 +0300

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -1,21 +1,32 @@
 const products = [
+
     {
         id: 1001,
+
         name: "Xiaomi 17 Ultra",
+
         category: "Смартфоны",
+
         memory: "16 / 512 GB",
+
         color: "Black",
+
         display: 1,
+
         warehouse: 2,
+
         quantity: 3,
-        description: "Флагманский смартфон Xiaomi 17 Ultra.",
+
+        description:
+            "Флагманский смартфон Xiaomi 17 Ultra.",
+
         specs: {
             "Память": "16 / 512 GB",
             "Цвет": "Black"
         },
-        tip: "Один экземпляр на витрине, два на складе."
-    }
-];
 
-console.log("PRODUCT-DATA OK");
-console.log(products);
+        tip:
+            "Один экземпляр находится на витрине, два — на складе."
+    }
+
+];
